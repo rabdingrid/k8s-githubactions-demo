619e1c1f15a435c14cc2daec84cf23e474fe0a5b
rabdingrid <rabdin@griddynamics.com>
2025-06-19 13:46:36 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello from Kubernetes + GitHub Actions!'));
+app.listen(3000, () => console.log('Server running on port 3000'));
+
